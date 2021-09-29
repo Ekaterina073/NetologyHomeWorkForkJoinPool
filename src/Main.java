e3dd7531f3d2a554f0b8b699920d4af9591679e0
botkoda <botkoda@gmail.com>
2021-09-29 14:01:56 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+    public static void main(String[] args) {
+        long m = System.currentTimeMillis();
+        int SIZE=17000;
+        int[] arr=new int[SIZE];
+        randomArr(arr);
+        int result=sumArr(arr);
+        System.out.println("Результат:" +result);
+        System.out.println("Продолжительность (мс)" + (double) (System.currentTimeMillis()-m));
+    }
+
+
+
+    private static int sumArr(int[] arr) {
+        if(arr.length==0) return 0;
+        return arr[0]+ sumArr(Arrays.copyOfRange(arr,1,arr.length));
+    }
+
+    private static void randomArr(int[] arr) {
+        Random rd =new Random();
+        for (int i=0;i<arr.length;i++){
+            arr[i]=rd.nextInt(100);
+            System.out.println(arr[i]);
+        }
+    }
+
+
+}
---
+++ src/MainFork.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class MainFork {
+    public static void main(String[] args) {
+        long m = System.currentTimeMillis();
+        int nThreads = Runtime.getRuntime().availableProcessors();
+        //размер массива
+        int SIZE=100000;
+        int[] arr=new int[SIZE];
+        randomArr(arr);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(nThreads);
+        Integer result = forkJoinPool.invoke(new Sum(arr,0,arr.length));
+
+        System.out.println("Результат:" + result);
+        System.out.println("Продолжительность (мс)" + (double) (System.currentTimeMillis()-m));
+    }
+    static class Sum extends RecursiveTask<Integer>{
+        int low;
+        int high;
+        int[] array;
+
+        Sum(int[] array, int low, int high) {
+            this.array = array;
+            this.low   = low;
+            this.high  = high;
+        }
+        @Override
+        protected Integer compute() {
+            if(high - low <= 10) {
+                int sum = 0;
+
+                for(int i = low; i < high; ++i)
+                    sum += array[i];
+                return sum;
+            } else {
+                int mid = low + (high - low) / 2;
+                Sum left  = new Sum(array, low, mid);
+                Sum right = new Sum(array, mid, high);
+                left.fork();
+                int rightResult = right.compute();
+                int leftResult  = left.join();
+                return leftResult + rightResult;
+            }
+        }
+    }
+
+    private static void randomArr(int[] arr) {
+        Random rd = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.println(arr[i]);
+        }
+    }
+}
